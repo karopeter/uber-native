48360d7b90f5ab50a46cb01f89c68068244af577
karopeter <karopeter531@gmail.com>
2022-07-12 16:56:08 -0700

    clone uberApp

---
+++ components/navOptions.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import { FlatList, TouchableOpacity, Image, Text, View } from 'react-native';
+import tw from "tailwind-react-native-classnames";
+import { Icon } from "react-native-elements";
+import { useNavigation } from "@react-navigation/native";
+
+const data = [
+    {
+     id: "123",
+     title: "Get a ride",
+     image: "https://links.papareact.com/3pn",
+     screen: "MapScreen"
+    },
+    {
+     id: "456",
+     title: "Order food",
+     image: "https://links.papareact.com/28w",
+     screen: "EatsScreen" // Change in future... 
+    },
+];
+
+const NavOptions = () => {
+    const navigation = useNavigation();
+
+    return (
+      <FlatList 
+        data={data}
+        keyExtractor={(item) => item.id}
+        horizontal
+        renderItem={({ item }) => (
+          <TouchableOpacity 
+            onPress={() => navigation.navigate(item.screen)}
+            style={tw`p-2 pl-6 pb-8 pt-4 bg-gray-200 h-60 m-2 mt-10 w-40`}
+           >
+             <View>
+                <Image 
+                  style={{ 
+                    width: 120, 
+                    height: 120,
+                    resizeMode: 'contain',
+                 }}
+                 source={{ uri: item.image }} 
+                 />
+                 <Text style={tw`text-sm font-semibold`}>{item.title}</Text>
+                 <Icon 
+                   style={tw`p-2 bg-black rounded-full w-10 mt-4`}
+                   name="arrowright" 
+                   color="white" 
+                   type="antdesign" 
+                   />
+             </View>
+          </TouchableOpacity> 
+        )}
+      />
+    );
+}
+
+export default NavOptions;
